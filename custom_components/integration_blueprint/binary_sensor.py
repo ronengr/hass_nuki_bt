4de4d3c05d0892d8d9ed56710902cac036522a98
Ronen Gruengras <ronen@gruengras.com>
2023-04-01 23:54:05 +0300

    Rename integration_blueprint to hass_nuki_bt

--- custom_components/integration_blueprint/binary_sensor.py
+++ custom_components/integration_blueprint/binary_sensor.py
@@ -1,4 +1,4 @@
-"""Binary sensor platform for integration_blueprint."""
+"""Binary sensor platform for hass_nuki_bt."""
 from __future__ import annotations
 
 from homeassistant.components.binary_sensor import (
@@ -13,8 +13,8 @@ from .entity import IntegrationBlueprintEntity
 
 ENTITY_DESCRIPTIONS = (
     BinarySensorEntityDescription(
-        key="integration_blueprint",
-        name="Integration Blueprint Binary Sensor",
+        key="hass_nuki_bt",
+        name="Nuki BT Binary Sensor",
         device_class=BinarySensorDeviceClass.CONNECTIVITY,
     ),
 )
@@ -33,7 +33,7 @@ async def async_setup_entry(hass, entry, async_add_devices):
 
 
 class IntegrationBlueprintBinarySensor(IntegrationBlueprintEntity, BinarySensorEntity):
-    """integration_blueprint binary_sensor class."""
+    """hass_nuki_bt binary_sensor class."""
 
     def __init__(
         self,
