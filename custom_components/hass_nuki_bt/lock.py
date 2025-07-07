5a43048052db44a7a544a6c1af23dd5aa781928c
Artem Sheremet <dot.doom@gmail.com>
2025-07-07 22:53:17 +0200

    Allow picking target for update_nuki_time action

--- custom_components/hass_nuki_bt/lock.py
+++ custom_components/hass_nuki_bt/lock.py
@@ -1,10 +1,12 @@
 """Library to handle connection with Nuki Lock."""
 import logging
+import voluptuous as vol
 from typing import Any
 from homeassistant.components.lock import LockEntity, LockEntityFeature
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.core import HomeAssistant, SupportsResponse
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import entity_platform
 from pyNukiBT import NukiConst, NukiDevice, NukiLockConst, NukiOpenerConst
 
 from .entity import NukiEntity
@@ -14,9 +16,14 @@ from .const import DOMAIN
 
 logger = logging.getLogger(__name__)
 
+UPDATE_NUKI_TIME_SERVICE_NAME = "update_nuki_time"
+# Has to be a simple dictionary to be extended with "target" parameters.
+UPDATE_NUKI_TIME_SCHEMA = {
+    vol.Optional("time"): cv.datetime,
+}
 
 async def async_setup_entry(
-    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: entity_platform.AddEntitiesCallback
 ) -> None:
     """Set up Nuki lock based on a config entry."""
     coordinator: NukiDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
@@ -24,6 +31,13 @@ async def async_setup_entry(
         async_add_entities([NukiOpener(coordinator)])
     else:
         async_add_entities([NukiLock(coordinator)])
+    platform = entity_platform.async_get_current_platform()
+    platform.async_register_entity_service(
+        UPDATE_NUKI_TIME_SERVICE_NAME,
+        schema=UPDATE_NUKI_TIME_SCHEMA,
+        func="async_handle_update_nuki_time",
+        supports_response=SupportsResponse.OPTIONAL,
+    )
 
 
 class NukiLock(NukiEntity, LockEntity):
